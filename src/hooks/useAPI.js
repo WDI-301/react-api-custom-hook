97c171a997971adb4a155ed250a2d76ce6cc4f88
Michael Dwyer <michael.dwyer@codeimmersives.com>
2023-03-23 08:38:18 -0700

    added async/await in comments

--- src/hooks/useAPI.js
+++ src/hooks/useAPI.js
@@ -5,9 +5,20 @@ const useAPI = (route, id) => {
   useEffect(() => {
     const baseUrl = 'https://jsonplaceholder.typicode.com'
     fetch(`${baseUrl}/${route}/${id}`)
-      .then(response => console.log(response.json()))
+      .then(response => response.json())
+      .then(json => console.log(json))
       .catch(error => console.log(error))
-  }, [route, id])
+ 
+    // async/await
+    // const fetchData = async () => {
+    // const response = await fetch(`${baseUrl}/${route}/${id}`)
+    // const responseJSON = await response.json()
+    // console.log(responseJSON)
+    // }
+    // fetchData()
+
+}, [route, id])
+
   
 
 
